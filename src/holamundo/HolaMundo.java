9684e798c9a987c9241bdf29ea39fbe5374a03ee
Gerardo Ibarra <gerardoibarra2110@gmail.com>
2021-02-16 17:16:12 -0600

    Primera Versionom

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,16 @@
+/**
+ * El objetivo de este proyecto es crear un programa sencillo que mande un mensaje 
+ * Todo esto con el objetivo de subir este mismo proyecto a GitHub
+ * @author Gerardo Ibarra "TokyoYaroslavl"
+ * v1.0 :   16-Febrero-2021.
+ * 
+*/
+package holamundo;
+
+public class HolaMundo 
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hola Mundo");
+    }
+}
